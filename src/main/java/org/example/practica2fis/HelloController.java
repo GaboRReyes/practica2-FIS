dc041763daa599560c6d4576018f91778a8ab13f
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2024-09-17 18:43:33 -0600

    Ultimos ajustes a la GUI

--- src/main/java/org/example/practica2fis/HelloController.java
+++ src/main/java/org/example/practica2fis/HelloController.java
@@ -1,15 +1,14 @@
 package org.example.practica2fis;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import org.w3c.dom.ls.LSOutput;
 
 import java.net.URL;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
@@ -17,13 +16,17 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     @FXML
     private ComboBox<String> cmb_tipo_persona = new ComboBox<>();
+    @FXML
+    Button salir;
     private ArrayList<String> tipos = new ArrayList<>(Arrays.asList("Profesor", "Egresado", "Familiar"));
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> itemsList = FXCollections.observableArrayList(tipos);
 
         cmb_tipo_persona.setItems(itemsList);
-
-
     }
+    @FXML public void onSalirBoton() {
+        Platform.exit();
+    }
+
 }
